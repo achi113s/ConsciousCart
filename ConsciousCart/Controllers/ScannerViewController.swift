9cddd4a502ad2ecf01ec1e7f0417f1b1f2dbd7ad
achi113s <latourgiorgio@gmail.com>
2023-05-01 23:39:24 -0400

    add base file for barcode scanner view controller

---
+++ ConsciousCart/Controllers/ScannerViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ScannerViewController.swift
+//  ConsciousCart
+//
+//  Created by Giorgio Latour on 5/1/23.
+//
+
+import UIKit
+
+class ScannerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
