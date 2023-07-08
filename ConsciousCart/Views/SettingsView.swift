7cc73a81a84d5394df69e39839469bf77e189288
achi113s <latourgiorgio@gmail.com>
2023-07-08 16:28:29 -0400

    a ton of stuff, including work on profile, settings views

--- ConsciousCart/Views/SettingsView.swift
+++ ConsciousCart/Views/SettingsView.swift
@@ -15,34 +15,34 @@ struct SettingsView: View {
     @State private var forceDarkModeSetting = UserDefaults.standard.bool(forKey: UserDefaultsKeys.forceDarkModeSetting.rawValue)
     
     var body: some View {
-        VStack {
-            Form {
-                Section("Appearance") {
-                    List {
-                        Toggle("Dark Mode", isOn: $forceDarkModeSetting)
-                            .onChange(of: forceDarkModeSetting) { darkModeEnabled in
-                                UserDefaults.standard.set(forceDarkModeSetting, forKey: UserDefaultsKeys.forceDarkModeSetting.rawValue)
-                            }
-                    }
+        List {
+            Section("Appearance") {
+                VStack {
+                    Toggle("Dark Mode", isOn: $forceDarkModeSetting)
+                        .onChange(of: forceDarkModeSetting) { darkModeEnabled in
+                            UserDefaults.standard.set(forceDarkModeSetting, forKey: UserDefaultsKeys.forceDarkModeSetting.rawValue)
+                        }
                 }
-                
-                Section("My Data") {
-                    List {
-                        Button("Delete My Data", role: .destructive) {
-                            showingDeleteAlert = true
-                        }
-                        .alert("Delete My Data", isPresented: $showingDeleteAlert) {
-                            Button("Cancel", role: .cancel) { }
-                            Button("Delete", role: .destructive) {
-                                impulsesStateManager?.deleteAllImpulses()
-                            }
-                        } message: {
-                            Text("Are you sure you want to permanently delete all of your Impulses? This action cannot be undone.")
-                        }
-                        
+                .font()
+            }
+            
+            Section("My Data") {
+                VStack {
+                    Button("Delete My Data", role: .destructive) {
+                        showingDeleteAlert = true
                     }
+                    .alert("Delete My Data", isPresented: $showingDeleteAlert) {
+                        Button("Cancel", role: .cancel) { }
+                        Button("Delete", role: .destructive) {
+                            impulsesStateManager?.deleteAllImpulses()
+                        }
+                    } message: {
+                        Text("Are you sure you want to permanently delete all of your Impulses? This action cannot be undone.")
+                    }
+                    
                 }
             }
         }
+        .listStyle(.insetGrouped)
     }
 }
