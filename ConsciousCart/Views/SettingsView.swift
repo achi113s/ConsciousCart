5ce280b3118ce2289f67a332d5cb698155548ed4
achi113s <latourgiorgio@gmail.com>
2023-07-08 17:06:32 -0400

    fixed bug with animation of navigation bar title from ConsciousCart to next view

--- ConsciousCart/Views/SettingsView.swift
+++ ConsciousCart/Views/SettingsView.swift
@@ -8,96 +8,85 @@
 import CoreData
 import SwiftUI
 
+// This view needs work because List does not support Buttons
+// and the gestures get all messed up.
 struct SettingsView: View {
-//    var impulsesStateManager: ImpulsesStateManager?
+    var impulsesStateManager: ImpulsesStateManager?
     
     @State private var showingDeleteAlert = false
     @State private var forceDarkModeSetting = UserDefaults.standard.bool(forKey: UserDefaultsKeys.forceDarkModeSetting.rawValue)
     
     var body: some View {
-        ScrollView {
-            VStack(alignment: .leading) {
-                Section("About") {
+        List {
+            Section("About") {
+                Group {
                     Button() {
                         
                     } label: {
                         Text("📒  About ConsciousCart")
                     }
-                    .buttonStyle(.borderedProminent)
+                    
+                    Button() {
+                        
+                    } label: {
+                        HStack {
+                            Text("📧  Share Feedback")
+                            Spacer()
+                            Image(systemName: "arrow.up.forward")
+                        }
+                    }
+                    
+                    Button() {
+                        
+                    } label: {
+                        HStack {
+                            Text("💫  Rate ConsciousCart!")
+                            Spacer()
+                            Image(systemName: "arrow.up.forward")
+                        }
+                    }
                 }
+                .font(Font.custom("Nunito-Semibold", size: 17))
+                .foregroundColor(.black)
             }
+            .font(Font.custom("Nunito-Semibold", size: 13))
+            
+            Section("Appearance") {
+                VStack {
+                    Toggle("🌙  Dark Mode", isOn: $forceDarkModeSetting)
+                        .onChange(of: forceDarkModeSetting) { darkModeEnabled in
+                            UserDefaults.standard.set(forceDarkModeSetting, forKey: UserDefaultsKeys.forceDarkModeSetting.rawValue)
+                        }
+                }
+                .font(Font.custom("Nunito-Semibold", size: 17))
+            }
+            .font(Font.custom("Nunito-Semibold", size: 13))
+            
+            Section("My Data") {
+                VStack {
+                    Button("Delete My Data", role: .destructive) {
+                        showingDeleteAlert = true
+                    }
+                    .alert("Delete My Data", isPresented: $showingDeleteAlert) {
+                        Button("Cancel", role: .cancel) { }
+                        Button("Delete", role: .destructive) {
+                            impulsesStateManager?.deleteAllImpulses()
+                        }
+                    } message: {
+                        Text("Are you sure you want to permanently delete all of your Impulses? This action cannot be undone.")
+                    }
+                    
+                }
+                .font(Font.custom("Nunito-Semibold", size: 17))
+            }
+            .font(Font.custom("Nunito-Semibold", size: 13))
         }
+        .listStyle(.insetGrouped)
     }
-        //        List {
-        //            Section("About") {
-        //                Group {
-        //                    Button() {
-        //
-        //                    } label: {
-        //                        Text("📒  About ConsciousCart")
-        //                    }
-        //
-        //                    Button() {
-        //
-        //                    } label: {
-        //                        HStack {
-        //                            Text("📧  Share Feedback")
-        //                            Spacer()
-        //                            Image(systemName: "arrow.up.forward")
-        //                        }
-        //                    }
-        //
-        //                    Button() {
-        //
-        //                    } label: {
-        //                        HStack {
-        //                            Text("💫  Rate ConsciousCart!")
-        //                            Spacer()
-        //                            Image(systemName: "arrow.up.forward")
-        //                        }
-        //                    }
-        //                }
-        //                .font(Font.custom("Nunito-Semibold", size: 17))
-        //                .foregroundColor(.black)
-        //            }
-        //            .font(Font.custom("Nunito-Semibold", size: 13))
-        //
-        //            Section("Appearance") {
-        //                VStack {
-        //                    Toggle("🌙  Dark Mode", isOn: $forceDarkModeSetting)
-        //                        .onChange(of: forceDarkModeSetting) { darkModeEnabled in
-        //                            UserDefaults.standard.set(forceDarkModeSetting, forKey: UserDefaultsKeys.forceDarkModeSetting.rawValue)
-        //                        }
-        //                }
-        //                .font(Font.custom("Nunito-Semibold", size: 17))
-        //            }
-        //            .font(Font.custom("Nunito-Semibold", size: 13))
-        //
-        //            Section("My Data") {
-        //                VStack {
-        //                    Button("Delete My Data", role: .destructive) {
-        //                        showingDeleteAlert = true
-        //                    }
-        //                    .alert("Delete My Data", isPresented: $showingDeleteAlert) {
-        //                        Button("Cancel", role: .cancel) { }
-        //                        Button("Delete", role: .destructive) {
-        //                            impulsesStateManager?.deleteAllImpulses()
-        //                        }
-        //                    } message: {
-        //                        Text("Are you sure you want to permanently delete all of your Impulses? This action cannot be undone.")
-        //                    }
-        //
-        //                }
-        //                .font(Font.custom("Nunito-Semibold", size: 17))
-        //            }
-        //            .font(Font.custom("Nunito-Semibold", size: 13))
-        //        }
-        //        .listStyle(.insetGrouped)
-        //    }
-    }
+}
 
 struct SettingsView_Previews: PreviewProvider {
     static var previews: some View {
-        SettingsView()
+        SettingsView(impulsesStateManager: nil)
     }
 }
